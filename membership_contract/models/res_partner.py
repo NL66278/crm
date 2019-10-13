bc2b80e7616cfdab8be2129639265663f23fed1b
Ronald Portier <ronald@therp.nl>
2019-10-13 20:01:30 +0200

    [IMP] Let personal and associate membership coexist peacefully.

--- membership_contract/models/res_partner.py
+++ membership_contract/models/res_partner.py
@@ -36,21 +36,22 @@ _logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    membership = fields.Boolean(
-        string='Is member?',
-        readonly=True)
+    membership = fields.Boolean(string="Is member?", readonly=True)
+    personal_member = fields.Boolean(string="Is personal member?", readonly=True)
     associate_member = fields.Many2one(  # no _id to be compatible with std.
-        comodel_name='res.partner',
-        string='Member via partner',
+        comodel_name="res.partner",
+        string="Member via partner",
         readonly=True,
-        index=True)
+        index=True,
+    )
     membership_line_ids = fields.One2many(
-        comodel_name='account.analytic.invoice.line',
-        inverse_name='partner_id',
-        domain=[('membership', '=', True)],
-        string='Membership contract lines')
+        comodel_name="account.analytic.invoice.line",
+        inverse_name="partner_id",
+        domain=[("membership", "=", True)],
+        string="Membership contract lines",
+    )
 
     @api.multi
     def membership_change_trigger(self):
@@ -62,45 +63,41 @@ class ResPartner(models.Model):
 
     @api.multi
     def _compute_membership(self):
-        """Compute wether partner is a direct member.
-
-        Dependent modules might compute membership through association
-        with members determined here. Therefore membership will not be
-        updated if member through association.
-        """
-        today = fields.Date.context_today(self)
+        """Compute wether partner is a member."""
         for this in self:
-            save_membership = this.membership
-            membership = False
-            for line in this.membership_line_ids:
-                # Check wether line belongs to active contract
-                contract = line.analytic_account_id
-                if ((contract.date_start and contract.date_start > today) or
-                        (contract.date_end and contract.date_end <= today)):
-                    continue  # not active contract
-                membership = True
-                break
-            if not membership and this.associate_member:
-                # Leave membership through association alone.
-                continue
-            if membership != save_membership:
-                vals = {'membership': membership}
+            membership = this._is_member()
+            if membership != this.membership:
                 if membership:
-                    # clear associate membership, if now direct member.
-                    vals['associate_member'] = False
-            if membership != save_membership:
-                if save_membership:
-                    _logger.info(
-                        _('%s is no longer a member'), this.display_name)
+                    _logger.info(_("%s is now a member"), this.display_name)
                 else:
-                    _logger.info(_('%s is now a member'), this.display_name)
-                this.write(vals)
+                    _logger.info(_("%s is no longer a member"), this.display_name)
+                this.write({"membership": membership})
                 this.membership_change_trigger()
+
+    @api.multi
+    def _is_member(self):
+        """Check personal membership.
+
+        Might be extended for other forms of membership.
+        """
+        self.ensure_one()
+        today = fields.Date.context_today(self)
+        for line in self.membership_line_ids:
+            # Check wether line belongs to active contract
+            contract = line.analytic_account_id
+            if (contract.date_start and contract.date_start > today) or (
+                contract.date_end and contract.date_end <= today
+            ):
+                continue  # not active contract
+            self.personal_member = True
+            return True
+        self.personal_member = False
+        return False
 
     @api.multi
     def write(self, vals):
         result = super(ResPartner, self).write(vals)
-        if 'membership' in vals:
+        if "membership" in vals:
             self.membership_change_trigger()
         return result
 
--- membership_contract_hierarchy/models/res_partner.py
+++ membership_contract_hierarchy/models/res_partner.py
@@ -1,50 +1,46 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# pylint: disable=protected-access
 from odoo import api, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.multi
     def membership_change_trigger(self):
         """Compute membership for members immediately below this one."""
-        hierarchy_model = self.env['res.partner.relation.hierarchy']
+        super(ResPartner, self).membership_change_trigger()
+        hierarchy_model = self.env["res.partner.relation.hierarchy"]
         for this in self:
-            partners_below = hierarchy_model.search([
-                ('partner_above_id', '=', this.id),
-                ('level', '=', 1)])
+            partners_below = hierarchy_model.search(
+                [("partner_above_id", "=", this.id), ("level", "=", 1)]
+            )
             for partner_below in partners_below:
                 partner_below.partner_below_id._compute_membership()
 
     @api.multi
-    def _compute_membership(self):
-        # First compute direct membership in super.
+    def _is_member(self):
+        """Check associate membership.
+
+        Can exist alongside personal membership.
+        """
+        self.ensure_one()
         self.env.invalidate_all()  # Prevent stale partner_above_ids.
-        super(ResPartner, self)._compute_membership()
-        for this in self:
-            if this.membership and not this.associate_member:
-                # direct member.
+        super_member = super(ResPartner, self)._is_member()
+        # Check all partners above us for membership.
+        for partner_above in self.partner_above_ids:
+            associate = partner_above.partner_above_id
+            if not associate.membership:
                 continue
-            save_membership = this.membership
-            membership = False
-            # Might still be member through partner above
-            for partner_above in this.partner_above_ids:
-                associate = partner_above.partner_above_id
-                if not associate.membership:
-                    continue
-                membership = True
-                # Only write real changes
-                if not this.membership or \
-                        this.associate_member != associate:
-                    super(ResPartner, this).write({
-                        'membership': True,
-                        'associate_member': associate.id})
-                    break
-            if not membership and this.membership:
-                super(ResPartner, this).write({
-                    'membership': False,
-                    'associate_member': False})
-            if membership != save_membership:
-                this.membership_change_trigger()
+            # We have a member above us, so we are member too.
+            # Only write real changes
+            if not self.membership or self.associate_member != associate:
+                super(ResPartner, self).write(
+                    {"membership": True, "associate_member": associate.id}
+                )
+            return True
+        # If we get here, we are not member through hierarchy.
+        super(ResPartner, self).write({"associate_member": False})
+        return super_member
